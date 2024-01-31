b30f7d8ee861f258cbdf2baf21f10517e4def26e
Ned Lane <nedlane06@icloud.com>
2024-01-31 12:20:29 +1000

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { ReCaptchaProvider } from "next-recaptcha-v3";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <ReCaptchaProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ReCaptchaProvider>
     </ThemeProvider>
   );
