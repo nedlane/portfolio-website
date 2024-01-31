763d13a468e9de8f9399eb17fa0866f42737e7de
Ned Lane <nedlane06@icloud.com>
2024-01-31 12:28:50 +1000

    Added speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ReCaptchaProvider } from "next-recaptcha-v3";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       <ReCaptchaProvider>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </ReCaptchaProvider>
     </ThemeProvider>
   );
