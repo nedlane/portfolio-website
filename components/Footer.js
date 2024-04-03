11389d93a2e59f33d846a57fec8f67246b044cbc
Ned Lane <nedlane06@icloud.com>
2024-04-03 11:35:53 +1000

    Update copyright© notice

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,9 @@ export default function Footer() {
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
           <div>
-            <p>&copy; Ned Lane. All Rights Reserved.</p>
+            <p>
+              &copy; Ned Lane {new Date().getFullYear()}. All Rights Reserved.
+            </p>
           </div>
           <div className="space-x-4 flex flex-row items-center">
             <a
